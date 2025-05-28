7229dac953fd18a489832c4065cd766364ca1217
DESKTOP-FBB0HLE\user <pss9203@ipartners.co.kr>
2025-05-28 11:01:53 +0900

    수정완료 작업

--- src/App.js
+++ src/App.js
@@ -18,13 +18,12 @@ function App() {
         todo.id === id ? { ...todo, done: !todo.done } : todo
       )
     );
-    console.log(todos)
   };
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData(prev => {
       const updated = { ...prev, [name]: value };
-      console.log(updated);
+      //console.log(updated);
       return updated;
     });
     
@@ -42,22 +41,45 @@ function App() {
     };
     setTodos(prev => [...prev, newTodo]);
     setFormData({ id:nextId, type: formData.type, title: formData.title, text: formData.text });
-    console.log(setTodos)
   }
 
   const buttonModify = (todo) => {
-    console.log('수정',todo)
-    setModify(todo)
-  }
-  const buttonDel = (id) => {
-    const target = todos.find(todo => todo.id === id);
-    if(target && target.done===false){
-      alert('체크박스 선택해야함')
+    if (todo && todo.done === false) {
+      alert('체크박스 선택해야함');
       return;
     }
+    console.log('수정',todo)
+    setModify(todo)
+    setFormData({
+      type: todo.type,
+      title: todo.title,
+      text: todo.text,
+    });
+  }
+
+  const applyModify = () => {
+    if (!isModify) return;
+
     setTodos(prevTodos =>
-      prevTodos.filter(todo => todo.id !== id)
-      
+      prevTodos.map(todo =>
+        todo.id === isModify.id
+          ? { ...todo, ...formData }
+          : todo
+      )
+    );
+    //수정완료 후 초기화
+    setModify(null);
+    setFormData({ type: '', title: '', text: '' });
+  };
+  const buttonDel = (todo) => {
+    console.log(todo)
+    if (todo && todo.done === false) {
+      alert('체크박스 선택해야함')
+      return
+    }
+
+    setTodos(prevTodos =>
+      prevTodos.filter(item => item.id !== todo.id) // 현재 항목의 ID가 삭제하려는 항목의 ID와 같지 않으면(true) 이 항목 남기고 아닌거 삭제
     )
   }
 
@@ -92,13 +114,25 @@ function App() {
             onClick={()=> buttonModify(todo) } 
           >수정</button>
           <button type="button"
-            onClick={()=> buttonDel(todo.id)}
+            onClick={()=> buttonDel(todo)}
           >삭제</button>
         </div>
         
     ))}
     {isModify &&(
-      <div>123</div>
+      <div className="wrap_add">
+        <select title="타입 선택" name="type" value={formData.type} onChange={handleInputChange}>
+          <option value="">타입을 선택하세요</option>
+          {types.map((type) =>(
+            <option value={type}>{type}</option>
+          ))}
+        </select>
+        <input type="text" name="title" placeholder="타이틀 입력"onChange={handleInputChange} />
+        <input type="text" name="text" placeholder="설명 입력"onChange={handleInputChange} />
+        <button type="button"
+            onClick={()=> applyModify() } 
+          >수정완료</button>
+      </div>
     )}
     </div>
   );
